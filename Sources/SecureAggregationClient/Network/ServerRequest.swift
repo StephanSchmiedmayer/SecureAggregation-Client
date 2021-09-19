4ea78680d36460541fe162846982047f4c158867
Stephan Schmiedmayer <ge25bih@mytum.de>
2021-09-19 21:27:56 +0200

    Change ServerRequest internal workings, extend ServerRequest API

--- Sources/SecureAggregationClient/Network/ServerRequest.swift
+++ Sources/SecureAggregationClient/Network/ServerRequest.swift
@@ -25,32 +25,67 @@ class ServerRequestHandler {
         self.serverBaseURL = serverBaseURL
         self.urlsession = URLSession(configuration: .default)
     }
-    
-//    func requestIgnoringResponse<BodyType: Encodable>(for endpoint: SABasicAPI, body: BodyType) {
-//        guard let encodedBody = try? JSONEncoder().encode(body) else {
-//            logger.network(endpoint: endpoint, "Failed to encode data for server Request")
-//            return
-//        }
-//        requestIgnoringResponse(for: endpoint, body: encodedBody)
-//    }
+
+    // MARK: - API
+    // So many different functions and not one with optionals because optionals would be from Generic type, and from nil no Type can be inferred => :(
     
     /// Sends a request to the server, ignoring any successful response from the server. Logs any error
     ///
-    /// modelAccess gets called to load the body of the request
-    ///
-    func requestIgnoringReponse<BodyType: Encodable>(for endpoint: SABasicAPI,
-                                                     modelToNetwork: @escaping () throws -> BodyType) {
+    /// `modelAccess` gets called to load the body of the request
+    func requestIgnoringReponse<BodyType: SANetworkMessage>(for endpoint: SABasicAPI,
+                                                            modelToNetwork: @escaping () throws -> BodyType) {
         do {
-            let encodedBody = try JSONEncoder().encode(try modelToNetwork())
-            requestIgnoringResponse(for: endpoint, body: encodedBody)
+            request(try createRequest(for: endpoint, withBody: try modelToNetwork()),
+                    task: ignoreServerResponse(fromEndpoint: endpoint))
         } catch {
-            logger.network(endpoint: endpoint, "Failed to access model / encode data for server Request: \(error)")
+            logger.network(endpoint: endpoint, "Failed send request: \(error)")
         }
     }
         
     /// Makes a request to the server disregarding the response, logging any error
-    func requestIgnoringResponse(for endpoint: SABasicAPI, body: Data? = nil) {
-        request(for: endpoint, withBody: body) { result in
+    func requestIgnoringResponse(for endpoint: SABasicAPI) {
+        request(createRequest(for: endpoint), task: ignoreServerResponse(fromEndpoint: endpoint))
+    }
+        
+    /// Sends an empty request to the given endpoint.
+    ///
+    /// Catches and logs any error
+    ///
+    /// - Parameters:
+    ///     - endpoint: The endpoint to send the request to
+    ///     - decodeInto: Type to decode the server response into
+    ///     - callToMethod: Closure executed with the decoded result from the server. Guaranteed to be called from the main Queue
+    func request<DecoderType: Decodable>(for endpoint: SABasicAPI,
+                                         decodeInto decoderType: DecoderType.Type,
+                                         callToModel: @escaping (DecoderType) throws -> Void) {
+        request(createRequest(for: endpoint), task: decodeAndCallToModel(endpoint: endpoint,
+                                                                         decodeInto: decoderType,
+                                                                         callToModel: callToModel))
+    }
+        
+    func request<DecoderType: Decodable, BodyType: SANetworkMessage>(
+        for endpoint: SABasicAPI,
+        decodeInto decoderType: DecoderType.Type,
+        body: BodyType,
+        callToModel: @escaping (DecoderType) throws -> Void) {
+        do {
+            request(try createRequest(for: endpoint, withBody: body),
+                    task: decodeAndCallToModel(endpoint: endpoint,
+                                               decodeInto: decoderType,
+                                               callToModel: callToModel))
+        } catch {
+            logger.network(endpoint: endpoint, "Failed send request: \(error)")
+        }
+    }
+    
+    // MARK: - private Helper
+    
+    // MARK: Standard server response handler
+    
+    /// Function returning a function usable to ignore the server response as `task` in `request(_:task:)`
+    private func ignoreServerResponse(fromEndpoint endpoint: SABasicAPI) ->
+    ((AnyPublisher<Data, Publishers.TryMap<URLSession.DataTaskPublisher, Data>.Failure>) -> AnyPublisher<Void, Never>) {
+        { result in
             result
                 .map { _ in
                     ()
@@ -63,19 +98,11 @@ class ServerRequestHandler {
         }
     }
     
-    /// Sends an empty request to the given endpoint.
-    ///
-    /// Catches and logs any error
-    ///
-    /// - Parameters:
-    ///     - endpoint: The endpoint to send the request to
-    ///     - decodeInto: Type to decode the server response into
-    ///     - callToMethod: Closure executed with the decoded result from the server. Guaranteed to be called from the main Queue
-    func request<DecoderType: Decodable>(for endpoint: SABasicAPI,
-                                         body: Data? = nil,
-                                         decodeInto decoderType: DecoderType.Type,
-                                         callToModel: @escaping (DecoderType) throws -> Void) {
-        request(for: endpoint, withBody: body) { result in
+    private func decodeAndCallToModel<DecoderType: Decodable>(endpoint: SABasicAPI,
+                                                              decodeInto decoderType: DecoderType.Type,
+                                                              callToModel: @escaping (DecoderType) throws -> Void) ->
+    ((AnyPublisher<Data, Publishers.TryMap<URLSession.DataTaskPublisher, Data>.Failure>) -> AnyPublisher<Void, Never>) {
+        { result in
             result
                 .decode(type: DecoderType.self, decoder: JSONDecoder())
                 .receive(on: DispatchQueue.main)
@@ -89,19 +116,28 @@ class ServerRequestHandler {
                 .eraseToAnyPublisher()
         }
     }
+
+    // MARK: Create request
+    
+    /// Creates an URLRequest to the endpoint with the given body
+    private func createRequest<BodyType: Encodable>(for endpoint: SABasicAPI, withBody body: BodyType) throws -> URLRequest {
+        guard let bodyData = try? JSONEncoder().encode(body) else {
+            logger.network(endpoint: endpoint, "Failed to JSON-encode \(BodyType.Type.self)")
+            throw SecureAggregationNetworkError.encodingError
+        }
+        var result = createRequest(for: endpoint)
+        result.httpBody = bodyData
+        result.addValue("application/json", forHTTPHeaderField: "content-type")
+        return result
+    }
         
-    private func createRequest(for endpoint: SABasicAPI, withBody body: Data? = nil) -> URLRequest {
+    private func createRequest(for endpoint: SABasicAPI) -> URLRequest {
         var result = URLRequest(url: serverBaseURL.appendingPathComponent(endpoint.info.fullRelativeURL))
         result.httpMethod = endpoint.info.method.rawValue
-        result.httpBody = body
         return result
     }
     
-    private func request(for endpoint: SABasicAPI,
-                         withBody body: Data? = nil,
-                         task: @escaping (AnyPublisher<Data, Publishers.TryMap<URLSession.DataTaskPublisher, Data>.Failure>) -> AnyPublisher<Void, Never>) {
-        request(createRequest(for: endpoint, withBody: body), task: task)
-    }
+    // MARK: Make actual request
     
     /// Makes a new Request to the server.
     private func request(_ request: URLRequest, task: @escaping (AnyPublisher<Data, Publishers.TryMap<URLSession.DataTaskPublisher, Data>.Failure>) -> AnyPublisher<Void, Never>) {
